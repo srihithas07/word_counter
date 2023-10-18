e40f4c28d754a5667287fe112b7f8657f71cd69f
srihithas07 <134121789+srihithas07@users.noreply.github.com>
2023-10-18 12:13:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-
 import './App.css';
 import { Paragraph } from './components/Paragraph';
 
 function App() {
   return (
     <div>
-      <h2 style={{color:"black"}}>React-Task-1</h2>
+      <h2 style={{ color: "black" }}>React-Task-1</h2>
       <Paragraph />
     </div>
   );
 }
 
 export default App;
-
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
